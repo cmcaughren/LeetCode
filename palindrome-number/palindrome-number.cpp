2953c4101c0a6d6004b10db05b9c5e37623fb553
cmcaughren <50632501+cmcaughren@users.noreply.github.com>
2021-11-26 11:29:53 -0800

    Time: 99 ms (5.08%), Space: 5.8 MB (74.29%) - LeetHub

---
+++ palindrome-number/palindrome-number.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false; //negative number, not palindrome
+        if (x < 10) return true; //single digit, palindrome
+
+        int n = x;
+        int count = 0;
+        
+        //find the number of digits of x
+        while (n != 0){
+            n = n/10;
+            count ++;
+        }
+        
+        n = x;
+        int array[count];
+        bool palindrome = true;
+        
+        for (int i = (count - 1); i >= 0; i--){
+            array[i] = n%10;
+            n = n/10;
+        }
+        for (int i = 0; i < count; i++){
+            cout << array[i];
+        }
+        for (int i = 0; i < (count/2); i++){
+        
+            if (array[i] != array[(count -1 -i)]){
+                palindrome = false;
+            }
+        }
+        return palindrome;
+    }
+};
+
