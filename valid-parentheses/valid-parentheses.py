454e7a6615d50be9a0ffd4c2f3e4c9732bba806c
cmcaughren <50632501+cmcaughren@users.noreply.github.com>
2022-10-04 09:31:12 -0700

    Time: 54 ms, Memory: 13.9 MB - LeetHub

---
+++ valid-parentheses/valid-parentheses.py
@@ -0,0 +1,40 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        #nesting of brackets is allowed 
+        #use a stack
+        #string will be length 1 and greater
+        stack = []     
+        for bracket in s:
+            
+            if (bracket == "{") or (bracket == "(") or (bracket == "["):
+                stack.append(bracket)
+            else: #bracket is either }, ), or ]
+
+                if len(stack) == 0:
+                    return False
+                
+                i = stack.pop()
+                if i == "{":
+                    if bracket is not "}":
+                        return False
+                elif i == "(":
+                    if bracket is not ")":
+                        return False
+                elif i == "[":
+                    if bracket is not "]":
+                        return False
+                #we have a matching bracket set
+        print(stack)
+        if len(stack) != 0:
+            return False
+        else: 
+            return True       
+                    
+                
+            
+            
+            
+            
+            
+            
+        
