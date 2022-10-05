c7179f7a45310d81b06b6930089867d2288a6fde
cmcaughren <50632501+cmcaughren@users.noreply.github.com>
2022-10-05 09:28:08 -0700

    Time: 124 ms, Memory: 14.4 MB - LeetHub

--- valid-palindrome/valid-palindrome.py
+++ valid-palindrome/valid-palindrome.py
@@ -5,11 +5,11 @@ class Solution:
         s = s.lower()
         
         #cycle through each letter in s and remove all letters not alphanumeric
+        new_s = ""
         for l in s:
-            if not l.isalnum():
-                #replaces all instances of character l in s with "", not just those in the current position 
-                s = s.replace(l, "")
-        
+            if l.isalnum():
+                new_s = new_s + l 
+        s = new_s
         end = len(s) - 1
         for index, l in enumerate(s):
             if index >= (end - index):
